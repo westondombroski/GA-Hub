d3b05523e9b9ef5fbfec860bd9aa0ea9b29c693e
puan0601 <anton.pugach@gmail.com>
2017-03-28 13:34:17 -0700

    SEARCHWORKS!!!!!!!!!!!!"

--- generally_assembled/config/routes.rb
+++ generally_assembled/config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
+  get '/repos/search', to: 'repos#search', :as => :search
   resources :repos
   resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "users#index"
+  root to: "repos#index"
 
   get '/auth/github/callback', to: 'sessions#create'
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/users/new', to: 'users#new'
-  get '/repos', to: 'repos#index'
-  get '/repos/:id', to: 'repos#show'
 
 end
