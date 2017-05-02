1e054fd4d9a5ace8d5ae30e7f7854f879ed90113
westondombroski <westondombroski@gmail.com>
2017-05-01 21:48:35 -0700

    complete front-end overhaul

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "repos#index"
+  root to: "users#index"
 
   get '/auth/github/callback', to: 'sessions#create'
   get '/signout' => 'sessions#destroy', :as => :signout
-  get '/users/new', to: 'users#new'
 
   get '/search', to: 'repos#search'
 
+  get '/users', to: 'users#index'
 end
